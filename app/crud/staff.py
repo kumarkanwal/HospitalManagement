c984865a16bdaece297352a77fa471660631e796
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2026-07-13 03:49:12 -0700

    feat: Add staff crud

---
+++ app/crud/staff.py
@@ -0,0 +1,44 @@
+from sqlalchemy.orm import Session
+
+from app.models.staff import Staff
+from app.schemas.staff import StaffCreate
+
+
+def create_staff(db: Session, staff: StaffCreate):
+    db_staff = Staff(
+        name = staff.name,
+        role = staff.role,
+        shift = staff.shift,
+        salary = staff.salary
+
+    )
+
+    db.add(db_staff)
+    db.commit()
+    db.refresh(db_staff)
+    return db_staff
+
+def get_staff(db : Session, staff_id: int):
+    return db.query(Staff).filter(Staff.id == staff_id).first()
+
+def get_all_staff(db: Session, staff_id:int):
+    return db.query(Staff).all()
+
+def update_staff(db: Session, staff: StaffCreate):
+    db_staff = db.query(Staff).filter(Staff.id == staff_id).first()
+    db_staff.name = staff.name
+    db_staff.role = staff.role
+    db_staff.shift = staff.shift
+    db_staff.salary = staff.salary
+
+    db.commit()
+    db.refresh(db_staff)
+    return db_staff
+
+
+def delete_staff(db: Session, staff_id: int):
+    
+    db_staff = db.query(Staff).filter(Staff.id == staff_id).first()
+    db.delete(db_staff)
+    db.commit()
+    return {"message": "staff deleted Successfully"}
